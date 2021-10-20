eafc313d602f2364e5319fa34196116dc038c992
Rafal <ravmiecznk@gmail.com>
2021-10-20 11:10:28 +0200

    run server fix

--- aquaserver/run_server.sh
+++ aquaserver/run_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-this_path=$(dirname $0)
+this_path=$(dirname $(realpath $0))
 
 killall aquaserver.py
 
-$this_path/aquaserver.py &
+$this_path/aquaserver.py 2>&1 >> $this_path/aqualog.log &
 pid=$!
 nohup cpulimit --pid $pid --limit 50 &
