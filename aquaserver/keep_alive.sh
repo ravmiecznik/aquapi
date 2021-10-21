540c5b5191a0c63855fee7fad43e67ce43cd0789
rmiecznik <rmiecznik@magicleap.com>
2021-10-21 11:33:17 +0200

    keep_alive update

--- aquaserver/keep_alive.sh
+++ aquaserver/keep_alive.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-
 this_path=$(dirname $0)
+
+exec >> $this_path/keep_alive.log
+exec 2>&1
+
 ps -aux | grep aquaserver.py | grep -v grep
 
 status=$?
@@ -12,12 +15,18 @@ function run() {
 }
 
 function log () {
-	echo $*
+	echo $(date) $*
 	logger "$*"
 }
 
 if [ $status != 0 ]
 then
-	log Server not running, starting...
-	run nohup $this_path/run_server.sh
+  (
+    cd $this_path
+	  log Server not running, starting...
+	  killall -9 aquaserver.py
+    nohup ./aquaserver.py &
+    pid=$!
+    nohup cpulimit --pid $pid --limit 50 &
+	)
 fi
